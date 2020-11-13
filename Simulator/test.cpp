addfe683b0550ea026336d4a339e0349c19985a3
iannisdezwart <iannis__@hotmail.com>
2020-11-13 16:43:12 +0100

    🛠 Updated Call to simulate() to Use the New Arguments

--- Simulator/test.cpp
+++ Simulator/test.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+#include "../libs/fs.hpp"
 #include "simulator.hpp"
 
 using namespace std;
@@ -10,10 +11,10 @@ int main()
 		.POPULATION_SIZE = 1000,
 		.NUMBER_OF_COMMUNITIES = 12,
 		.HUMAN_MAX_VELOCITY = 5.0,
-		.HUMAN_SPREAD_PROBABILITY = 0.05,
+		.HUMAN_SPREAD_PROBABILITY = 0.5,
 		.HUMAN_SPREAD_RANGE = 20,
 		.HUMAN_INFECTION_DURATION = 100
 	};
 
-	simulate(simulation_settings);
+	simulate("output/123.conta", simulation_settings);
 }
