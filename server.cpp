efe943b6ecddf5075b2c1a8971feabeb0152a21a
iannisdezwart <iannis__@hotmail.com>
2020-11-13 16:39:56 +0100

    🚀 Created run_simulator Function & Fixed Bug

--- server.cpp
+++ server.cpp
@@ -79,16 +79,59 @@ void send_file(const Request& req, Response& res)
 	stream_file(file_path, file_entry.mime_type, res);
 }
 
+string random_hex_string()
+{
+	char byte_chars[3];
+	string output;
+	RandomIntGenerator<0x00, 0xFF> random_int;
+
+	for (int i = 0; i < 4; i++) {
+		uint8_t random_byte = random_int.generate();
+		sprintf(byte_chars, "%02x", random_byte);
+		output.push_back(byte_chars[0]);
+		output.push_back(byte_chars[1]);
+	}
+
+	return output;
+}
+
 void run_simulator(const Request& req, Response& res)
 {
-	// Todo: make
+	string run_id = random_hex_string();
 
-	// SimulationSettings settings = {
-		
-	// };
+	string POPULATION_SIZE_HEADER = req.get_header_value("population-size");
+	string NUMBER_OF_COMMUNITIES_HEADER = req.get_header_value("number-of-communities");
+	string HUMAN_MAX_VELOCITY_HEADER = req.get_header_value("human-max-velocity");
+	string HUMAN_SPREAD_PROBABILITY_HEADER = req.get_header_value("human-spread-probability");
+	string HUMAN_SPREAD_RANGE_HEADER = req.get_header_value("human-spread-range");
+	string HUMAN_INFECTION_DURATION_HEADER = req.get_header_value("human-infection-duration");
 
-	// thread simulation_thread = thread(simulate, settings);
-	// simulation_thread.join();
+	int POPULATION_SIZE = atoi(POPULATION_SIZE_HEADER.c_str());
+  int NUMBER_OF_COMMUNITIES = atoi(NUMBER_OF_COMMUNITIES_HEADER.c_str());
+  double HUMAN_MAX_VELOCITY = atof(HUMAN_MAX_VELOCITY_HEADER.c_str());
+  double HUMAN_SPREAD_PROBABILITY = atof(HUMAN_SPREAD_PROBABILITY_HEADER.c_str());
+  int HUMAN_SPREAD_RANGE = atoi(HUMAN_SPREAD_RANGE_HEADER.c_str());
+  int HUMAN_INFECTION_DURATION = atoi(HUMAN_INFECTION_DURATION_HEADER.c_str());
+
+	SimulationSettings settings = {
+		.POPULATION_SIZE = POPULATION_SIZE,
+		.NUMBER_OF_COMMUNITIES = NUMBER_OF_COMMUNITIES,
+		.HUMAN_MAX_VELOCITY = HUMAN_MAX_VELOCITY,
+		.HUMAN_SPREAD_PROBABILITY = HUMAN_SPREAD_PROBABILITY,
+		.HUMAN_SPREAD_RANGE = HUMAN_SPREAD_RANGE,
+		.HUMAN_INFECTION_DURATION = HUMAN_INFECTION_DURATION,
+	};
+
+	printf("Running a simulation with these settings:\n");
+
+	printf("POPULATION_SIZE: %d\n", POPULATION_SIZE);
+	printf("NUMBER_OF_COMMUNITIES: %d\n", NUMBER_OF_COMMUNITIES);
+	printf("HUMAN_MAX_VELOCITY: %f\n", HUMAN_MAX_VELOCITY);
+	printf("HUMAN_SPREAD_PROBABILITY: %f\n", HUMAN_SPREAD_PROBABILITY);
+	printf("HUMAN_SPREAD_RANGE: %d\n", HUMAN_SPREAD_RANGE);
+	printf("HUMAN_INFECTION_DURATION: %d\n", HUMAN_INFECTION_DURATION);
+
+	simulate("Simulator/output/" + run_id + ".conta", settings);
 }
 
 void list_runs(const Request& req, Response& res)
@@ -99,18 +142,18 @@ void list_runs(const Request& req, Response& res)
 	vector<string> file_names = fs::read_directory("Simulator/output");
 
 	for (int i = 0; i < file_names.size(); i++) {
-		fs::FileStats stats = fs::get_stats(file_names[i]);
+		fs::FileStats stats = fs::get_stats("Simulator/output/" + file_names[i]);
 		pair<string, fs::FileStats> entry(file_names[i], stats);
 		files.push_back(entry);
 	}
 
-	// Sort the files by creation time
+	// Sort the files by last modification time
 
 	sort(files.begin(), files.end(), [](
 		pair<string, fs::FileStats> f1,
 		pair<string, fs::FileStats> f2
 	) {
-		return f2.second.last_change_time - f1.second.last_change_time;
+		return f2.second.last_modification_time - f1.second.last_modification_time;
 	});
 
 	// Send the file names to the client
@@ -118,7 +161,7 @@ void list_runs(const Request& req, Response& res)
 	string output = "";
 
 	for (int i = 0; i < files.size(); i++) {
-		time_t timestamp = time(&files[i].second.last_change_time);
+		time_t timestamp = files[i].second.last_modification_time;
 		output += files[i].first + '\t' + to_string(timestamp) + '\n';
 	}
 
@@ -181,7 +224,10 @@ void get_run_output(const Request& req, Response& res)
 			sink.done();
 			return true;
 		},
-		[=]() { delete file; }
+		[=]() {
+			file->close();
+			delete file;
+		}
 	);
 }
 
