c2a261aebbac14776f501027c4b28b742407c6e4
iannisdezwart <iannis__@hotmail.com>
2020-11-14 21:17:02 +0100

    📑 Now Makes Use of the Newly Added Callback in /Simulator/simulator.hpp

--- WebApp/perform-run.ts
+++ WebApp/perform-run.ts
@@ -13,8 +13,24 @@ const performRun = () => {
 	req.setRequestHeader('human-spread-range', get('#human-spread-range'))
 	req.setRequestHeader('human-infection-duration', get('#human-infection-duration'))
 
+	req.addEventListener('loadstart', () => {
+		// Todo: this header does not get received, fix this
+
+		console.log(req.getResponseHeader('run-id'))
+	})
+
+	let receivedBytes = 0
+
 	req.addEventListener('readystatechange', () => {
-		console.log(req.response)
+		const response = req.response as string
+		if (response.length == 0) return
+
+		console.log(response.substring(receivedBytes))
+		receivedBytes = response.length
+	})
+
+	req.addEventListener('load', () => {
+		// console.log('Full response', req.response)
 	})
 
 	req.send()
