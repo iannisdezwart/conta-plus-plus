42081386d03784902bf2baef184b94b8555378b8
iannisdezwart <iannis__@hotmail.com>
2020-11-17 15:57:07 +0100

    🛠 Fixed Memory Leak & Simplified RandomIntGenerator Class

--- Simulator/random.hpp
+++ Simulator/random.hpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-template <uint64_t lower_bound, uint64_t upper_bound>
 class RandomIntGenerator {
 	private:
 		random_device device;
@@ -13,7 +12,7 @@ class RandomIntGenerator {
 		uniform_int_distribution<uint64_t> distribution;
 
 	public:
-		RandomIntGenerator()
+		RandomIntGenerator(uint64_t lower_bound, uint64_t upper_bound)
 		{
 			generator = mt19937(device());
 			distribution = uniform_int_distribution<uint64_t>(lower_bound, upper_bound);
@@ -23,26 +22,18 @@ class RandomIntGenerator {
 		{
 			return distribution(generator);
 		}
-
-		RandomIntGenerator& operator=(RandomIntGenerator const& other)
-		{
-			return *this = other;
-		}
 };
 
 class RandomFloatGenerator {
 	private:
-		RandomIntGenerator<0, 9223372036854775807> *random_int_generator;
+		RandomIntGenerator random_int_generator;
 
 	public:
-		RandomFloatGenerator()
-		{
-			random_int_generator = new RandomIntGenerator<0, 9223372036854775807>();
-		}
+		RandomFloatGenerator() : random_int_generator(0, 9223372036854775807) {}
 
 		double generate()
 		{
-			return (double) random_int_generator->generate() / 9223372036854775807.0;
+			return (double) random_int_generator.generate() / 9223372036854775807.0;
 		}
 };
 
